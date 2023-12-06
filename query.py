bad35db64d1d14d73b1b26b4bfb5b39fe794e8a6
Ivy Nguyen <nguyen.ivy1802@knights.ucf.edu>
2023-12-05 21:10:24 -0500

    adjusted databases

--- query.py
+++ query.py
@@ -4,5 +4,5 @@ from database import getQuery
 # the table is called transportationTable2, as part of the database: GKTWTransportationDataRes.db
 # you can query from here, or DBeaver (or anywhere you have SQLite)
 
-# getQuery('DROP TABLE IF EXISTS GKTWTransportationDataRes.transportationTable2')
-# getQuery('SELECT * FROM GKTWTransportationDataRes.transportationTable2')
+getQuery('DROP TABLE IF EXISTS transportationTable2')
+# getQuery('SELECT * FROM transportationTable2')
