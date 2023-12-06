9792ae5804c0d388fda83a5dff190c7e184775c6
Ivy Nguyen <nguyen.ivy1802@knights.ucf.edu>
2023-12-05 21:07:16 -0500

    cleared databases

--- query.py
+++ query.py
@@ -5,5 +5,5 @@ the table is called transportationTable, as part of the database: GKTWTransporta
 you can query from here, or DBeaver (or anywhere you have SQLite)
 '''
 
-# getQuery('DROP TABLE IF EXISTS GKTWTransportationData.transportationTable')
+getQuery('DROP TABLE IF EXISTS transportationTable')
 # getQuery('SELECT * FROM GKTWTransportationData.transportationTable')
